4f259099e43ef5c7a5c09979b122bbe3b27e898d
maxwell-wayne-17 <maxwell.wayne.17@cnu.edu>
2019-10-30 22:24:49 -0400

    stuff compiles

--- StringParserClass.cpp
+++ StringParserClass.cpp
@@ -11,7 +11,50 @@
 #include "../327_proj3_test/includes/constants.h"
 #include "../327_proj3_test/includes/StringParserClass.h"
 
-StringParserClass::StringParserClass():pStartTag(NULL), pEndTag(NULL), areTagsSet(false){}
+using namespace KP_StringParserClass;
+
+StringParserClass::StringParserClass(void):pStartTag(NULL), pEndTag(NULL), areTagsSet(false){}
+
+StringParserClass::~StringParserClass(void){
+
+	cleanup();
+
+}
+
+int StringParserClass::setTags(const char *pStart, const char *pEnd){
+	if (!pStart || !pEnd){
+		return ERROR_TAGS_NULL;
+	}
+
+	char start = *pStart;
+	char end = *pEnd;
+
+	this->pStartTag = &start;
+	this->pEndTag = &end;
+
+	return SUCCESS;
+}
+
+int StringParserClass::getDataBetweenTags(char *pDataToSearchThru, std::vector<std::string> &myVector){
+	return -1000;
+}
+
+
+void StringParserClass::cleanup(){
+	if (pStartTag){
+		delete[] pStartTag;
+	}
+	if (pEndTag){
+		delete[] pEndTag;
+	}
+}
+
+int StringParserClass::findTag(char *pTagToLookFor, char *&pStart, char *&pEnd){
+	return -1000;
+}
+
+
+
 
 
 
